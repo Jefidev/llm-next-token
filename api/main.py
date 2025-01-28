d481fd1bafcee34246aa79bbb56bdf8a1cb27057
Jérôme Fink <jerome.fink@unamur.be>
2025-01-28 09:09:46 +0100

    switch to llama3

--- api/main.py
+++ api/main.py
@@ -6,8 +6,8 @@ import torch
 from fastapi import FastAPI
 
 app = FastAPI()
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloomz-560m")
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloomz-560m")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B")
 
 
 def get_k_next_tokens(input_text, model, tokenizer, k=10):
