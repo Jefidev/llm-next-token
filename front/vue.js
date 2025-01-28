6b317fdcf450325bc6503948f54d90d984f74570
Ariel BASSO MADJOUKENG <ariel.bassomadjoukeng@unamur.be>
2025-01-28 14:20:24 +0100

    fix bug

--- front/vue.js
+++ front/vue.js
@@ -1,56 +1,69 @@
 async function getNextToken(sentence, k) {
-try {
-const response = await fetch('http://localhost:8000/next-token', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ sentence: sentence, k: k })
-});
+    try {
+        const response = await fetch('http://localhost:8000/next-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ sentence: sentence, k: k })
+        });
 
-const data = await response.json();
-return data;
-} catch (error) {
-console.error('Error:', error);
-throw error;
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
 }
-}
 
 
-document.getElementById('input_token').addEventListener('input', function() {  
-const sentence = document.getElementById('input_token').value;
-const wordCount = sentence.split(/\s+/).filter(word => word.length > 0).length;
+let typingTimeout;
 
-if (wordCount > 3) {
-k = 5;
-getNextToken(sentence, k)
-.then(data => {
-    document.getElementById('output_token').innerHTML = ''; 
-    for (let key in data) {
-        let value = data[key].toFixed(4);  
-        document.getElementById('output_token').innerHTML += 
-            `<span id='key${key}' onclick='addtext(this)'>${key}</span> : ${value}<br>`;
-    const elements = document.querySelectorAll("[id^='key']");
+document.getElementById('input_token').addEventListener('input', function () {
+    clearTimeout(typingTimeout); // Annule le précédent timeout si l'utilisateur continue de taper
 
-}
-            
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+    const sentence = document.getElementById('input_token').value;
+    const wordCount = sentence.split(/\s+/).filter(word => word.length > 0).length;
 
-
-
-} else {
-document.getElementById('output_token').innerHTML = '';
-}
+    if (wordCount > 3) {
+        // Définit un délai de 3 secondes après la dernière frappe
+        typingTimeout = setTimeout(() => {
+            const k = 5;
+            getNextToken(sentence, k)
+                .then(data => {
+                    document.getElementById('output_token').innerHTML = '';
+                    for (let key in data) {
+                        let value = data[key].toFixed(4);
+                        let percentage = (value * 100) + 10;
+                        let jauge_value = percentage.toFixed(2);
+                        document.getElementById('output_token').innerHTML +=
+                            `<div class="key_jauge"> 
+                                <span id='key' onclick='addtext(this)'>${key}</span> :  
+                                <div class="progress-container">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" 
+                                            style="width:${percentage}%" aria-valuemin="0" aria-valuemax="10"> 
+                                            ${jauge_value}%
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }, 1500); // Délai de 3 secondes
+    } else {
+        document.getElementById('output_token').innerHTML = '';
+    }
 });
 
 
 function addtext(element) {
-let text_area =  document.getElementById('input_token');
-text_area.value += '' + element.innerHTML;
+    let text_area = document.getElementById('input_token');
+    text_area.value += '' + element.innerHTML;
 
-text_area.dispatchEvent(new Event('input'));
+    text_area.dispatchEvent(new Event('input'));
 }
 
